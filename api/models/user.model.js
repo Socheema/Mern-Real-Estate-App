40e0eda2cbd7186fd09bd2ade5ea433076cd837f
azubuike <chimafrancis94@gmail.com>
2024-01-09 15:34:47 +0100

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+    },
+  },
+  { timestamp: true }
+);
+
+
+const User = mongoose.model("User", userSchema);
+export default User;
